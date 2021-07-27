460a98a40683ab24d4732946665e5d3cf6b4da14
Ioana Mirica <ioana.mirica@coolblue.nl>
2021-07-27 18:31:27 +0200

    Apply SRP refactorization

---
+++ ArdalisRating/PolicySerializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ArdalisRating
+{
+    public class PolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string policyName)
+        {
+            return JsonConvert.DeserializeObject<Policy>(policyName,
+                new StringEnumConverter());
+        }
+    }
+}
